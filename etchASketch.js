dd413eecc0259d1caf15c8f4b82c930f8742140f
zahrabateninia <zahrabatenin@gmail.com>
2023-04-11 13:51:37 +0330

    the container of grid and its items are programmed

---
+++ etchASketch.js
@@ -0,0 +1,33 @@
+function createGrid(rows, cols) {
+    const numPixels = 480 / rows; // 480 is the width of the container it is hardcoded
+    const container = document.createElement('div');
+    container.classList.add('grid-container');
+    container.style.width = "480px";
+    container.style.height = "480px";
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    container.style.justifyContent = 'center';
+    container.style.alignItems = 'center';
+    container.style.marginTop = '30px';
+
+    container.style.border = '3px solid black';
+    container.style.boxShadow = '3px 3px 3px gray';
+    
+  
+    for (let i = 0; i < rows * cols; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('grid-cell');
+      cell.style.flexBasis = `calc(100% / ${cols})`;
+      cell.style.margin='0';
+      cell.style.boxSizing = 'border-box';
+      cell.style.border = '1px solid black';
+      cell.style.height = `${numPixels}px`;
+      container.appendChild(cell);
+    }
+    document.body.appendChild(container);
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    createGrid(30, 30);
+  });
+  
