7a297bd6d6b9e167d3d559a05ccdc26162ef4e3a
zahrabateninia <zahrabatenin@gmail.com>
2023-04-16 13:38:31 +0330

    changePixels function works properly

--- etchASketch.js
+++ etchASketch.js
@@ -1,19 +1,25 @@
-function createGrid(rows) {
+//creates a grid
+const container = document.createElement('div');
+
+function createGrid(){
+container.classList.add('grid-container');
+container.style.width = "480px";
+container.style.height = "480px";
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap'; //very imp 
+container.style.marginLeft ='auto';//gird in the center of page
+container.style.marginRight ='auto';
+container.style.marginBottom = '60px';
+container.style.marginTop = '30px';
+container.style.border = '3px solid black';
+container.style.boxShadow = '3px 3px 4px black';
+container.style.backgroundColor = 'white';
+document.body.appendChild(container);//added at the bottom of body
+}
+
+
+function changePixels(rows) {
     const numPixels = 480 / rows; // 480 is the width of the container it is hardcoded
-    const container = document.createElement('div');
-    container.classList.add('grid-container');
-    container.style.width = "480px";
-    container.style.height = "480px";
-    container.style.display = 'flex';
-    container.style.flexWrap = 'wrap'; //very imp 
-    container.style.marginLeft ='auto';//gird in the center of page
-    container.style.marginRight ='auto';
-    container.style.marginBottom = '60px';
-    container.style.marginTop = '30px';
-    container.style.border = '3px solid black';
-    container.style.boxShadow = '3px 3px 4px black';
-    container.style.backgroundColor = 'white';
-    
     for (let i = 0; i < rows * rows; i++) { //if row =16 we need 256 square in the grid
       const cell = document.createElement('div');
       cell.classList.add('grid-cell');
@@ -37,21 +43,26 @@ function createGrid(rows) {
     
     }
     document.body.appendChild(container);//added at the bottom of body
-  }//end if func
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    createGrid(60);
-  });
-  
+  }  
+
   //if the button clicked , prompt
   function promptBtn(){
     let newRows = parseInt(prompt('How many pixels do you desire? '));
-      if (100 < rows){
+      if (100 < newRows){
         alert('Sorry, the number of squares per side CANNOT be more than 100');
       }else{
-
-        return createGrid(newRows);
+        container.innerHTML= '';
+        return changePixels(newRows);
       }
   
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    createGrid();
+  });
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    changePixels(16);
+  })
+
 
